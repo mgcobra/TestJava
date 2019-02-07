ce83a28b39b209ce22b6973564ad527739ef9045
Becca Paul <BenPaul1@benpaulcomputer.lan>
2019-02-07 20:30:02 +0000

    Initial commit

---
+++ app/src/main/java/com/example/testjava/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.testjava;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button addBtn = findViewById(R.id.AddBtn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            int res;
+            @Override
+            public void onClick(View v) {
+                EditText num1 = findViewById(R.id.Num1EditText);
+                EditText num2 = findViewById(R.id.Num2EditText);
+                TextView resultTextView = findViewById(R.id.ResTextView);
+
+                int n1 = Integer.parseInt(num1.getText().toString());
+                int n2 = Integer.parseInt(num2.getText().toString());
+                res += n1 + n2;
+                resultTextView.setText(res + "");
+            }
+        });
+
+    }
+}
