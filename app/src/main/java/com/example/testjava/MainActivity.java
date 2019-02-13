eb062f333cf92f245dcaef7e915fa57125a27ea5
mikeg <mikeg>
2019-02-13 12:30:31 +0000

    added toggle button

--- app/src/main/java/com/example/testjava/MainActivity.java
+++ app/src/main/java/com/example/testjava/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.testjava;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -22,9 +24,11 @@ public class MainActivity extends AppCompatActivity {
         Button addBtn = findViewById(R.id.AddBtn);
         final TextView resultTextView = findViewById(R.id.ResTextView);
         final Button togBtn = findViewById(R.id.togBtn);
+        Button btnURL = findViewById(R.id.btnURL);
 
         addBtn.setOnClickListener(new View.OnClickListener() {
             int res;
+
             @Override
             public void onClick(View v) {
                 EditText num1 = findViewById(R.id.Num1EditText);
@@ -39,20 +43,34 @@ public class MainActivity extends AppCompatActivity {
 
 
         togBtn.setOnClickListener(new View.OnClickListener() {
-            Boolean pic=false;
+            Boolean pic = false;
+
             @Override
             public void onClick(View v) {
                 ImageView imgView = findViewById(R.id.img1);
                 if (pic) {
                     imgView.setImageResource(R.drawable.front_side);
-                    pic=false;
+                    pic = false;
                     uRes("Front", resultTextView, "Show Grass", togBtn);
-                }
-                else {
+                } else {
                     imgView.setImageResource(R.drawable.grass);
                     pic = true;
                     uRes("Grass", resultTextView, "Show Front", togBtn);
                 }
+            }
+        });
+
+        btnURL.setOnClickListener(new View.OnClickListener() {
+
+            //String strURL = "http://www.google.com";
+            String strURL = "http://www.mikegiddins.co.uk/3D";
+            //String strURL = "http://www.go4directaccess.com";
+
+            @Override
+            public void onClick(View v) {
+                //Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.go4directaccess.com"));
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(strURL));
+                startActivity(browserIntent);
             }
         });
 
